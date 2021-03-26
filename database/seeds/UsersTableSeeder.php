673d5a432e97c2401a92bba1184ea30c8cd40ab1
tanhongit <phuongtan12357nguyen@gmail.com>
2021-03-26 20:17:35 +0700

    add seeder user

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('users')->insert([
+                'name' => "user{$i}",
+                'email' => "user{$i}@gmail.com",
+                'password' => bcrypt('User@123'),
+            ]);
+        }
+    }
+}
